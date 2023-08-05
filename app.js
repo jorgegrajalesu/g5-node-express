db998bfbe5883e36f8c90b194b866a7f5ae0ff9e
jorgegrajalesu <jegosystem@gmail.com>
2023-08-04 19:11:07 -0500

    040823

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 
-console.log('NodeJS - Express');
+// importar el paquete de express, para el webserver
+const express = require('express');
+// inicializar la aplicación con el archivo app.js
+const app = express();
+// para activar el puerto
+const puerto = 7475;
